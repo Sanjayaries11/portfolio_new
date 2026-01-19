fce467c4499280901a7db4b5d79a22f84d003809
SanjayKumar <sanjaykumar.s@afablez.com>
2026-01-19 18:31:25 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import styles from "../css/Navbar.module.css";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <div className={styles.go}>
+
+            </div>
+        </nav>
+    )
+}
