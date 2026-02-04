ec29eb26c37afd07a963f188a33246e725c3d3db
SanjayKumar <sanjaykumar.s@afablez.com>
2026-02-04 18:32:58 +0530

    changes made

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,19 +1,25 @@
+import Banner from "../pages/service/Banner";
+
 export default function Service() {
     return (
-        <section className="page">
-            <h1>Services</h1>
-            <p>
-                I provide end-to-end software solutions tailored for businesses and startups.
-            </p>
+        // <section className="page">
+        //     <h1>Services</h1>
+        //     <p>
+        //         I provide end-to-end software solutions tailored for businesses and startups.
+        //     </p>
 
-            <ul>
-                <li>🌐 Website Development (React, Next.js)</li>
-                <li>📱 Web & Mobile Applications</li>
-                <li>🏢 ERP & CRM Systems</li>
-                <li>🛒 E-Commerce Platforms</li>
-                <li>⚙️ API & Backend Development</li>
-                <li>🚀 Performance & Security Optimization</li>
-            </ul>
-        </section>
+        //     <ul>
+        //         <li>🌐 Website Development (React, Next.js)</li>
+        //         <li>📱 Web & Mobile Applications</li>
+        //         <li>🏢 ERP & CRM Systems</li>
+        //         <li>🛒 E-Commerce Platforms</li>
+        //         <li>⚙️ API & Backend Development</li>
+        //         <li>🚀 Performance & Security Optimization</li>
+        //     </ul>
+        // </section>
+
+        <>
+            <Banner />
+        </>
     )
 }
---
+++ src/pages/service/Banner.jsx
@@ -0,0 +1,15 @@
+import styles from "./css/Banner.module.css";
+
+export default function Banner() {
+    return (
+        <>
+            <section className={styles.home}>
+                <div className="container">
+                    <h1>Experienced Software Developer for Scalable Digital Solutions</h1>
+                    <p>With 2+ years of hands-on industry experience, I specialize in developing robust, maintainable, and performance-driven applications tailored to business needs.</p>
+
+                </div>
+            </section>
+        </>
+    )
+}
